b6692dceb0a029be32c873151d2b3b3bf1aa9542
alexikicommit <alexis.jeanjean@ynov.com>
2025-01-12 17:09:43 +0100

    wip: feat(lieu-details): create page lieu-details

--- src/app/main/components/lieu-details/lieu-details.component.ts
+++ src/app/main/components/lieu-details/lieu-details.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { AppComponent } from '../../../app.component'
+import { HttpCallsService } from '../../services/httpCalls.service'
+import { LieuDetailsResponse } from '../../../models/LieuModels'
+import { Tile } from '../../../models/Tile'
+import { Observable, tap, throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+import { Constants } from '../../Constants'
 
 @Component({
   selector: 'app-lieu-details',
@@ -7,13 +14,43 @@ import { ActivatedRoute } from '@angular/router'
   styleUrl: './lieu-details.component.scss'
 })
 export class LieuDetailsComponent implements OnInit {
-  public lieuId: string | null = ''
+  public lieuId: string = ''
+  public tiles: Tile[] = []
+  public lieuDetails: LieuDetailsResponse | null = null
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private appComponent: AppComponent,
+              private httpCallsService: HttpCallsService,
+              private constants: Constants,) {
+  }
 
-  public ngOnInit() {
-    this.lieuId = this.route.snapshot.paramMap.get('id')
+  public ngOnInit(): void {
+    this.lieuId = this.route.snapshot.paramMap.get('id')!;
 
-    console.log('LieuDetailsComponent initialized with lieuId:', this.lieuId)
+    this.getLieuDetails().subscribe({
+      next: (response: LieuDetailsResponse) => {
+        this.lieuDetails = response;
+        this.tiles = [
+          { img: this.lieuDetails.favorite_picture, cols: 2, rows: 2, color: 'transparent', class: 'principal-picture' },
+          { img: this.lieuDetails.images?.at(0)?.imageUrl ? this.lieuDetails.images.at(0)?.imageUrl : '', cols: 2, rows: 1, color: 'transparent', class: 'test' },
+          { img: this.lieuDetails.images?.at(1)?.imageUrl ? this.lieuDetails.images.at(1)?.imageUrl : '', cols: 2, rows: 1, color: 'transparent', class: 'test' }
+        ];
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement des détails du lieu:', err);
+      }
+    });
+  }
+
+  public getLieuDetails(): Observable<LieuDetailsResponse> {
+    this.appComponent.isLoading = true;
+    return this.httpCallsService.getLieuById(this.lieuId).pipe(
+      tap(() => this.appComponent.isLoading = false),
+      catchError((error) => {
+        console.error('get lieu details error:', error)
+        this.appComponent.isLoading = false
+        return throwError(() => error)
+      })
+    );
   }
 }
---
+++ src/app/models/Tile.ts
@@ -0,0 +1,7 @@
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  img: string | undefined;
+  class: string;
+}
